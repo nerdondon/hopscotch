f5c0663c5385ab06cac71ac8d9a5b242e245da08
nerdondon <willdon@nerdondon.com>
2021-09-15 08:23:03 +0000

    Add some text for generated docs and re-export skiplist

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
+//! # Hopscotch - a skip list implemented in rust
+//! What it says. Cuz it skips.
+
 pub mod skiplist;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// Re-export the SkipList struct and show at the top level of docs
+#[doc(inline)]
+pub use crate::skiplist::SkipList;
