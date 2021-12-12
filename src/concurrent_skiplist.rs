8a122c2e52914835d72b3231e68b8c63ebf824a0
nerdondon <willdon@nerdondon.com>
2021-12-12 22:20:43 +0000

    Fix `find_greater_or_equal_node` to return the found node directly instead of doing an additional call to `next` after stopping
    The additional call would allow time for a concurrent writer to write a new node
    and mess up the result we initially found

--- src/concurrent_skiplist.rs
+++ src/concurrent_skiplist.rs
@@ -493,13 +493,41 @@ impl<K: Ord + Debug, V: Clone> ConcurrentSkipList<K, V> {
     /// Return a reference to the first node with a key that is greater than or equal to the target
     /// key.
     fn find_greater_or_equal_node(&self, target: &K) -> Option<&SkipNode<K, V>> {
-        self.find_less_than_node(target).and_then(|prev_node| {
-            // We potentially found a node right before the first node with a key greater than or
-            // equal to the our target key. Move the pointer forward one time to check if we
-            // actually have the node we are looking for or if we hit the end of the levels without
-            // finding anything.
-            prev_node.next()
-        })
+        if self.is_empty() {
+            return None;
+        }
+
+        let mut current_node = self.head();
+        // Start iteration at the top of the skip list "towers" and iterate through pointers at the
+        // current level. If we skipped past our key, move down a level.
+        for level_idx in (0..self.height()).rev() {
+            // Get an optional of the next node
+            let mut maybe_next_node = current_node.next_at_level(level_idx);
+
+            while let Some(next_node) = maybe_next_node {
+                match next_node.key.as_ref().unwrap().cmp(target) {
+                    std::cmp::Ordering::Less => {
+                        current_node = next_node;
+                        maybe_next_node = next_node.next_at_level(level_idx);
+                    }
+                    std::cmp::Ordering::Equal | std::cmp::Ordering::Greater => {
+                        if level_idx == 0 {
+                            // We are at the bottom of the tower, so this is closest node greater
+                            // than or equal to our target.
+                            return Some(next_node);
+                        }
+
+                        // We found a node greater than or equal to our target. See if this is the
+                        // the first greatest node after our target by breaking and moving one
+                        // level down in the tower.
+                        break;
+                    },
+                }
+            }
+        }
+
+        // This is reached when the target is greater than all of the nodes in the skip list.
+        None
     }
 }
 
